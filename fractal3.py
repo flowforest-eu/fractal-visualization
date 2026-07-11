aed82fce22b36a6e18b16059bf84ee83672d6fd5
Rauni Lillemets <rauni.lillemets@gmail.com>
2026-07-12 01:45:52 +0300

    Added TODO comments

--- fractal3.py
+++ fractal3.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_tractrix_spiral(a=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=False, num_points=5000):
+def generate_tractrix_spiral(scale=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=False, num_points=5000):
     # Parameter t runs from 0 close to pi/2 to avoid tan(pi/2) division by zero
     t = np.linspace(0, np.pi/2 - 0.06, num_points)
     
     # Polar definitions
-    r = a * np.cos(t)
+    r = np.cos(t)
     theta = np.tan(t) - t
     
     # Convert to base Cartesian coordinates
     x_base = r * np.cos(theta)
     y_base = r * np.sin(theta)
 
-    # Move to shape origin
-    shift_x = x_base[0]
-    shift_y = y_base[0]
+    # Substract vector (1,0), which is position of tractrix at t=0. So then t=0 is at (0,0).
+    x_base = x_base - 1
 
-    x_base = x_base - shift_x
-    y_base = y_base - shift_y
+    # Scale
+    x_base = scale * x_base
+    y_base = scale * y_base
 
     # Rotate by 90 degrees (pi/2) to orient it like an upright question mark/fern; then rotate by angle
     angle_rad = np.radians(angle_deg)
@@ -27,52 +27,48 @@ def generate_tractrix_spiral(a=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=Fals
     y = x_base * np.sin(rotation_angle) + y_base * np.cos(rotation_angle)
 
     if mirror == True:
-        x = x[0] + (x[0] - x)
+        x = -x
     
-    # Move to correct location
+    # Move to requested location
     x = x + x_orig
     y = y + y_orig
 
     return x, y
 
 
-# TODO angle_deg, mirror
-def generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=False, t=0):
+def generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=False, t=0):
     # Polar definitions
-    r = a * np.cos(t)
+    r = np.cos(t)
     theta = np.tan(t) - t
     
     # Convert to base Cartesian coordinates
     x_base = r * np.cos(theta)
     y_base = r * np.sin(theta)
 
-    # Calculate origin at t=0
-    r0 = a * np.cos(0)
-    theta0 = np.tan(0)
-    x0 = r0 * np.cos(theta0)
-    y0 = r0 * np.sin(theta0)
+    # Substract vector (1,0), which is position of tractrix at t=0. So then t=0 is at (0,0).
+    x_base = x_base - 1
 
-    # Move to shape origin
-    x_base = x_base - x0
-    y_base = y_base - y0
+    # Scale by a
+    x_base = scale * x_base
+    y_base = scale * y_base
 
     # Rotate by 90 degrees (pi/2) to orient it like an upright question mark/fern
     angle_rad = np.radians(angle_deg)
-
     rotation_angle = np.pi / 2 - angle_rad
     x = x_base * np.cos(rotation_angle) - y_base * np.sin(rotation_angle)
     y = x_base * np.sin(rotation_angle) + y_base * np.cos(rotation_angle)
 
     if mirror == True:
-        x = x[0] + (x[0] - x)
+        x = -x
 
-    # Move to correct location
+    # Move to requested location
     x = x + x_orig
     y = y + y_orig
 
     return x, y
 
 
+# TODO add mirror?
 def get_direction_angle(t, angle_deg=0, mirror=False):
     # Using small epsilon for finite difference to get tangent
     eps = 1e-5
@@ -96,50 +92,64 @@ def get_direction_angle(t, angle_deg=0, mirror=False):
     
     return phi % 360
 
+
+# TODO: add struct with x_or, y_or, mir, alpha, a
+# method: generate_tractrix_spiral(curve_params) -> curve = Class([x, y]; curve_params)
+# method: calculate_tractrix_data_at_point(curve, t) -> (x, y, alpha)
+
 # Generate the coordinates
-x, y = generate_tractrix_spiral(a=1.0, x_orig=0, y_orig=0, angle_deg=0)
+### TODO: curve1 = generate_tractrix_spiral(curve_params)
+x, y = generate_tractrix_spiral(scale=1.0, x_orig=0, y_orig=0, angle_deg=0)
 
 # Plot the curve
-# plt.figure(figsize=(6, 8), dpi=100)
+plt.figure(figsize=(6, 8), dpi=100)
 plt.plot(x, y, color='black', linewidth=2, solid_capstyle='round')
 
-print()
-
 # Second curve
-x0, y0 = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=0)
-# x1, y1 = generate_tractrix_point(a=1.0, x0=0, y0=0, t=1.35)
-angle_deg = get_direction_angle(t=1.41, angle_deg=0, mirror=False)
-x1, y1 = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.41)
-x, y = generate_tractrix_spiral(a=1.0, x_orig=x1-x0, y_orig=y1-y0, mirror=True, angle_deg=angle_deg)
+# TODO: generate_tractrix_data_at_point(curve1, t=1.35)
+x1, y1 = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.35)
+angle_deg = get_direction_angle(t=1.35, angle_deg=0, mirror=False)
+### TODO: curve1.params -> curve_params2
+### curve2 = generate_tractrix_spiral(curve_params2)
+x, y = generate_tractrix_spiral(scale=1.0, x_orig=x1, y_orig=y1, mirror=True, angle_deg=angle_deg)
+plt.plot(x, y, color='black', linewidth=2, solid_capstyle='round')
+
+### Third curve
+# TODO: generate_tractrix_data_at_point(curve2, t=1.4)
+xn1, yn1 = generate_tractrix_point(scale=1.0, x_orig=x1, y_orig=y1, mirror=True, angle_deg=angle_deg, t=1.4)
+angle_deg_new = get_direction_angle(t=1.4, angle_deg=angle_deg, mirror=True)
+### TODO: curve2.params -> curve_params3
+### curve3 = generate_tractrix_spiral(curve_params3)
+x, y = generate_tractrix_spiral(scale=1.0, x_orig=xn1, y_orig=yn1, mirror=False, angle_deg=angle_deg_new)
 plt.plot(x, y, color='black', linewidth=2, solid_capstyle='round')
 
 # Plot origin
 plt.plot(0, 0, 'go', markersize=3)
 
 # Plot some points
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.27)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.27)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.29)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.29)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.31)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.31)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.33)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.33)
 plt.plot(pointx, pointy, 'bo', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.35)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.35)
 plt.plot(pointx, pointy, 'bo', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.37)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.37)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.38)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.38)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.39)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.39)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.4)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.4)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.41)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.41)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.42)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.42)
 plt.plot(pointx, pointy, 'ro', markersize=6)
-pointx, pointy = generate_tractrix_point(a=1.0, x_orig=0, y_orig=0, t=1.43)
+pointx, pointy = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.43)
 plt.plot(pointx, pointy, 'ro', markersize=6)
 
 # Format the image to be a perfect base for the fractal mapping

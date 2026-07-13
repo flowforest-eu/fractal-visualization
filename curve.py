74f0b34c4909b6689eeb2c9ea31486b6f65abe05
Rauni Lillemets <rauni.lillemets@gmail.com>
2026-07-13 03:51:47 +0300

    moved to more OO approach

--- curve.py
+++ curve.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Curve:
     def __init__(self, x_orig, y_orig, scale, angle_deg, mirror):
         self.x_orig = x_orig
@@ -5,3 +8,54 @@ class Curve:
         self.scale = scale
         self.angle_deg = angle_deg
         self.mirror = mirror
+        self.spiral_generated = False
+        self.num_points = -1
+        self.x = []
+        self.y = []
+
+
+    def generate_spiral(self, num_points=5000):
+        # Parameter t runs from 0 close to pi/2 to avoid tan(pi/2) division by zero
+        t = np.linspace(0, np.pi/2 - 0.06, num_points)
+        
+        # Polar definitions
+        r = np.cos(t)
+        theta = np.tan(t) - t
+        
+        # Convert to base Cartesian coordinates
+        x_base = r * np.cos(theta)
+        y_base = r * np.sin(theta)
+
+        # Substract vector (1,0), which is position of tractrix at t=0. So then t=0 is at (0,0).
+        x_base = x_base - 1
+
+        # Scale
+        x_base = self.scale * x_base
+        y_base = self.scale * y_base
+
+        # Rotate by 90 degrees (pi/2) to orient it like an upright question mark/fern; then rotate by angle
+        angle_rad = np.radians(self.angle_deg)
+        rotation_angle = np.pi / 2 - angle_rad
+        x = x_base * np.cos(rotation_angle) - y_base * np.sin(rotation_angle)
+        y = x_base * np.sin(rotation_angle) + y_base * np.cos(rotation_angle)
+
+        if self.mirror == True:
+            x = -x
+        
+        # Move to requested location
+        x = x + self.x_orig
+        y = y + self.y_orig
+
+        self.x = x
+        self.y = y
+        self.spiral_generated = True
+        self.num_points = num_points
+
+        return self.x, self.y
+    
+
+    def get_spiral(self):
+        if self.spiral_generated == False:
+            raise Exception("Spiral not generated!")
+        
+        return self.x, self.y
--- fractal.py
+++ fractal.py
@@ -3,46 +3,6 @@ import matplotlib.pyplot as plt
 from curve import Curve
 
 
-# TODO: move to part of Curve
-def generate_tractrix_spiral_from_curve(curve):
-    return generate_tractrix_spiral(scale=curve.scale, x_orig=curve.x_orig, y_orig=curve.y_orig, angle_deg=curve.angle_deg, mirror=curve.mirror, num_points=5000)
-
-
-def generate_tractrix_spiral(scale=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=False, num_points=5000):
-    # Parameter t runs from 0 close to pi/2 to avoid tan(pi/2) division by zero
-    t = np.linspace(0, np.pi/2 - 0.06, num_points)
-    
-    # Polar definitions
-    r = np.cos(t)
-    theta = np.tan(t) - t
-    
-    # Convert to base Cartesian coordinates
-    x_base = r * np.cos(theta)
-    y_base = r * np.sin(theta)
-
-    # Substract vector (1,0), which is position of tractrix at t=0. So then t=0 is at (0,0).
-    x_base = x_base - 1
-
-    # Scale
-    x_base = scale * x_base
-    y_base = scale * y_base
-
-    # Rotate by 90 degrees (pi/2) to orient it like an upright question mark/fern; then rotate by angle
-    angle_rad = np.radians(angle_deg)
-    rotation_angle = np.pi / 2 - angle_rad
-    x = x_base * np.cos(rotation_angle) - y_base * np.sin(rotation_angle)
-    y = x_base * np.sin(rotation_angle) + y_base * np.cos(rotation_angle)
-
-    if mirror == True:
-        x = -x
-    
-    # Move to requested location
-    x = x + x_orig
-    y = y + y_orig
-
-    return x, y
-
-
 def generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, angle_deg=0, mirror=False, t=0):
     # Polar definitions
     r = np.cos(t)
@@ -100,14 +60,9 @@ def get_direction_angle(t, angle_deg=0, mirror=False):
     return phi % 360
 
 
-# TODO: Add [x,y] to class itself?
-# method: generate_tractrix_spiral(curve_params) -> curve = Class([x, y]; curve_params)
-# method: calculate_tractrix_data_at_point(curve, t) -> (x, y, alpha)
-
 # Generate the coordinates
-### TODO: curve1 = generate_tractrix_spiral(curve_params)
 curve1 = Curve(x_orig=0, y_orig=0, scale=1.0, angle_deg=0, mirror=False)
-x, y = generate_tractrix_spiral_from_curve(curve1)
+x, y = curve1.generate_spiral()
 
 # Plot the curve
 plt.figure(figsize=(6, 8), dpi=100)
@@ -117,18 +72,18 @@ plt.plot(x, y, color='black', linewidth=2, solid_capstyle='round')
 # TODO: generate_tractrix_data_at_point(curve1, t=1.35)
 x1, y1 = generate_tractrix_point(scale=1.0, x_orig=0, y_orig=0, t=1.35)
 angle_deg = get_direction_angle(t=1.35, angle_deg=0, mirror=False)
-### TODO: curve1.params -> curve_params2
-### curve2 = generate_tractrix_spiral(curve_params2)
-x, y = generate_tractrix_spiral(scale=1.0, x_orig=x1, y_orig=y1, mirror=True, angle_deg=angle_deg)
+# TODO: curve1.branch_at_point(...)
+curve2 = Curve(x_orig=x1, y_orig=y1, mirror=True, angle_deg = angle_deg, scale=1.0)
+x, y = curve2.generate_spiral()
 plt.plot(x, y, color='black', linewidth=2, solid_capstyle='round')
 
 ### Third curve
 # TODO: generate_tractrix_data_at_point(curve2, t=1.4)
 xn1, yn1 = generate_tractrix_point(scale=1.0, x_orig=x1, y_orig=y1, mirror=True, angle_deg=angle_deg, t=1.4)
 angle_deg_new = get_direction_angle(t=1.4, angle_deg=angle_deg, mirror=True)
-### TODO: curve2.params -> curve_params3
-### curve3 = generate_tractrix_spiral(curve_params3)
-x, y = generate_tractrix_spiral(scale=1.0, x_orig=xn1, y_orig=yn1, mirror=False, angle_deg=angle_deg_new)
+# TODO: curve1.branch_at_point(...)
+curve3 = Curve(x_orig=xn1, y_orig=yn1, mirror=False, angle_deg=angle_deg_new, scale=1.0)
+x, y = curve3.generate_spiral()
 plt.plot(x, y, color='black', linewidth=2, solid_capstyle='round')
 
 # Plot origin
